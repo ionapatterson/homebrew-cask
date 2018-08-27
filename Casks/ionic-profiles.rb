aca611391a7f3c5986c9596bff338ffa94c44614
apatterson <apatterson@ionicsecurity.com>
2018-08-27 14:37:14 -0700

    add appcast stanza

--- Casks/ionic-profiles.rb
+++ Casks/ionic-profiles.rb
@@ -3,6 +3,7 @@ cask 'ionic-profiles' do
   sha256 'b7e38ba243e6330c1e833d953f4f2c88579fd8136f75b6d688aff29640fb8dfc'
 
   url 'https://github.com/IonicDev/ionic-profiles/releases/download/2.2.0-307/ionic-profiles_macOS.zip'
+  appcast 'https://github.com/IonicDev/ionic-profiles/releases.atom'
   name 'ionic-profiles'
   homepage 'https://dev.ionic.com/tools/ionic-profiles'
 
